cb3d5e56bf811e95f90f5dd0f40939ea6852d152
BrunoLeal215 <bruno.leal.seb@gmail.com>
2016-04-03 18:21:29 -0300

    dois

--- app/assets/javascripts/tracking.js
+++ app/assets/javascripts/tracking.js
@@ -1,15 +1,50 @@
 $(document).on('ready page:load', function() 
 {	
-	var d = new Date,
-    currentdate = [d.getMonth()+1,
-               d.getDate(),
-               d.getFullYear()].join('/')+' '+
-              [d.getHours(),
-               d.getMinutes(),
-               d.getSeconds()].join(':');
+	var visitorID = '';
+	
+	//generate uuid	
+	function generateGUID(){
+		var data = new Date().getTime();
+		var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+			var r = (data + Math.random()*16)%16 | 0;
+			data = Math.floor(data/16);
+			return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+		});
+		visitorID = uuid;
+		//create cookie
+		document.cookie = "guid="+uuid;
+		console.log(document.cookie);
+	}
+ 
+	if(document.cookie.length !=0)
+	{
+		visitorID = document.cookie.split("=")[1];
+	}else
+	{
+		generateGUID();
+	}
+	
+	
+	$('a').click(function(e){
 
-	console.log(currentdate);
-	var url = window.location.href;
-	console.log(window.location.href);
-	$.ajax({url: '/rastrear', method: 'POST', data: { tracking: { url: url, guid: 'teste',data: currentdate}}});
+		var d = new Date,
+			currentdate = [d.getMonth()+1,
+					   d.getDate(),
+					   d.getFullYear()].join('/')+' '+
+					  [d.getHours(),
+					   d.getMinutes(),
+					   d.getSeconds()].join(':');
+
+			console.log(currentdate);
+			var url = $(this).text();
+			console.log($(this).text());
+			$.ajax({
+				url: '/rastrear', 
+				method: 'POST', 
+				data: { tracking: { url: url, guid: visitorID, data: currentdate}},
+				
+			});
+	});
+
+	
 });
